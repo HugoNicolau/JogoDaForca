8a703938fd78426784e4bdc1c9c2ffe4af2b62f1
hugo <nicolau.hugogiles@gmail.com>
2022-09-26 17:25:01 -0300

    Bugs corrigidos e com Final do jogo completo, também com Reiniciar jogo funcionando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import palavras from "./palavras";
-import React from "react"
+import React from "react";
 
 export default function App() {
   const alfabeto = [
@@ -31,64 +31,181 @@ export default function App() {
     "z",
   ];
 
-console.log("teste")
-const [botoes, setBotoes] = React.useState(false)
-const [erros, setErros] = React.useState(0)
-const [palavraEscolhida, setPalavra] = React.useState("")
+  const [botoes, setBotoes] = React.useState(false);
+  const [erros, setErros] = React.useState(0);
+  const [palavraEscolhida, setPalavra] = React.useState("");
+  const [palavraChute, setPalavraChute] = React.useState("");
+  const [fimDeJogo, setFimDeJogo] = React.useState(false);
+  const [acertos, setAcertos] = React.useState(0);
+  const [arrayLetras, setArrayLetras] = React.useState([]);
+  
+  console.log(acertos, "voce acertou isso de letras")
+  function iniciarJogo() {
+    if(botoes === false){
 
-const [arrayLetras,setArrayLetras] = React.useState([])
-function iniciarJogo(){
-    setBotoes(!botoes)
-    setErros(0)
-
-    alert("fui clicado")
-   let index =  Math.floor(Math.random() * palavras.length);
-   setPalavra(palavras[index])
-    
-
-}
-function marcarLetra(letra){
-
-
-const novoArray = [...arrayLetras, letra]
-setArrayLetras(novoArray)
-console.log(arrayLetras)
-
-if(palavraEscolhida.includes(letra)){
-    console.log("voce acertou")
-}
-else{
-    setErros(erros+1)
-}
-}
-function verificarLetra(letraClicada){
-    if(arrayLetras.includes(letraClicada)){
-
-        return `${letraClicada}`
+        setBotoes(!botoes);
     }
-    else{
-       return " _"
-    } 
-}
+    setErros(0);
+    setAcertos(0);
+    setArrayLetras([])
+    setClassesResult("jogo-iniciado")
+    setFimDeJogo(false)
+    setPalavraChute("")
+
+
+    
+    let index = Math.floor(Math.random() * palavras.length);
+    setPalavra(palavras[index]);
+  }
+
+  function marcarLetra(letra) {
+    if (letra === "a") {
+      const novoArray = [...arrayLetras, letra, "à", "ã", "â", "á"];
+      setArrayLetras(novoArray);
+    } else if (letra === "e") {
+      const novoArray = [...arrayLetras, letra, "é", "è", "ê", "ẽ"];
+      setArrayLetras(novoArray);
+    } else if (letra === "i") {
+      const novoArray = [...arrayLetras, letra, "í", "ì", "î", "ĩ"];
+      setArrayLetras(novoArray);
+    } else if (letra === "o") {
+      const novoArray = [...arrayLetras, letra, "õ", "ô", "ó", "ò"];
+      setArrayLetras(novoArray);
+    } else if (letra === "u") {
+      const novoArray = [...arrayLetras, letra, "ú", "ù", "û", "ũ"];
+      setArrayLetras(novoArray);
+    } else if (letra === "c") {
+      const novoArray = [...arrayLetras, letra, "ç"];
+      setArrayLetras(novoArray);
+    } else {
+      const novoArray = [...arrayLetras, letra];
+      setArrayLetras(novoArray);
+    }
+
+    console.log(arrayLetras + " arrayLetras");
+    console.log(palavraEscolhida + " palavraEscolhida");
+    console.log(palavraChute + " palavraChute");
+
+    if (
+      palavraEscolhida
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .includes(letra)
+    ) {
+      console.log(new Set(palavraEscolhida).size ,"voce acertou");
+      const novoAcerto = acertos+1;
+      setAcertos(novoAcerto);
+      if(new Set(palavraEscolhida.normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")).size === novoAcerto){
+        setClassesResult("jogo-iniciado ganhou")
+        setFimDeJogo(true)
+
+    }
+      
+    } else {
+        const novoErro = erros+1;
+      setErros(novoErro);
+      if(novoErro>5){
+        const novoArrayLetras = [...arrayLetras, ...palavraEscolhida.split("")]
+        setArrayLetras(novoArrayLetras)
+
+        setClassesResult("jogo-iniciado perdeu")
+        setFimDeJogo(true)
+        
+         
+      }
+    }
+  }
+  
+  function verificarLetra(letraClicada, idx, ary) {
+    
+      
+      if (arrayLetras.includes(letraClicada)) {
+        
+          return `${letraClicada}`;
+          
+        }
+        if (!arrayLetras.includes(letraClicada)) {
+            ary[idx] = "_";
+           
+            return " _";
+        }
+        
+    }
+    
+    
+  function chutarPalavra() {
+    if (palavraChute === palavraEscolhida) {
+      alert("Você ganhou");
+      setClassesResult("jogo-iniciado ganhou")
+      setArrayLetras(palavraEscolhida);
+      setFimDeJogo(true)
+
+    } else {
+      setErros(6);
+
+    setClassesResult("jogo-iniciado perdeu")
+    const novoArrayLetras = [...arrayLetras, ...palavraEscolhida.split("")]
+        setArrayLetras(novoArrayLetras)
+      alert("Você perdeu");
+      setFimDeJogo(true)
+
+    }
+  }
+  const [classesResult, setClassesResult] = React.useState("jogo-iniciado")
+
+  function mudarClasseResult(){
+    if(erros>5){
+        setClassesResult("jogo-iniciado perdeu")
+    }
+    
+  }
   return (
     <div className="container">
       <div className="parte-de-cima">
         <div className="forca">
-          <img src={`./assets/forca${erros}.png`} alt="imagem forca"></img>
+          <img data-identifier="game-image" src={`./assets/forca${erros}.png`} alt="imagem forca"></img>
         </div>
         <div className="escolher-palavra">
-          <button onClick={iniciarJogo}>Escolher palavra</button>
+          <button data-identifier="choose-word" onClick={iniciarJogo}>Escolher palavra</button>
         </div>
-        <div className="jogo-iniciado">{palavraEscolhida.split("").map((p)=> verificarLetra(p))}</div>
+        <div
+            data-identifier="word"
+          className={classesResult}
+        >
+          {palavraEscolhida
+            .split("")
+            .map((p, idx, ary) => verificarLetra(p, idx, ary))}
+        </div>
       </div>
       <div className="parte-de-baixo">
         <div className="letras">
-            {alfabeto.map((a) => <button onClick={()=> (marcarLetra(a))} disabled={!botoes || (arrayLetras.includes(a))}>{a.toUpperCase()}</button>)}
+          {alfabeto.map((a) => (
+            <button
+                
+              onClick={() => (marcarLetra(a), mudarClasseResult())}
+              data-identifier="letter"
+              disabled={!botoes || arrayLetras.includes(a) || fimDeJogo}
+            >
+              {a.toUpperCase()}
+            </button>
+          ))}
         </div>
         <div className="chutar-resposta">
-            Já sei a palavra! 
-        <input placeholder="" disabled={!botoes ? true : false}/>
-			<button disabled={!botoes ? true : false} onClick={()=> alert("Fui clicado")}>Chutar!</button>
+          Já sei a palavra!
+          <input
+            placeholder={palavraChute}
+            data-identifier="type-guess"
+            onChange={(e) => setPalavraChute(e.target.value)}
+            disabled={!botoes ? true : false || fimDeJogo}
+          />
+          <button
+            data-identifier="guess-button"
+            disabled={!botoes ? true : false}
+            onClick={() => chutarPalavra()}
+          >
+            Chutar!
+          </button>
         </div>
       </div>
     </div>
